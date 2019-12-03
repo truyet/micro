bc4c3e804d0d5f3459b7f3863ab3bc3f8a154fcd
Edward <chenlulei@gmail.com>
2019-12-03 22:38:43 +0800

    CORS Origin * by default on  commit e36582cd5561d174093815cd50637834cdca860e at 5 days ago (2019-11-28 5:09:01)

--- plugin.go
+++ plugin.go
@@ -2,10 +2,6 @@ package main
 
 import (
 	"io"
-	"net/http"
-
-	"github.com/micro/micro/api"
-	"github.com/micro/micro/plugin/micro/cors"
 )
 
 var (
@@ -22,18 +18,5 @@ func cleanWork() error {
 
 // 插件注册
 func init() {
-	// 跨域
-	initCors()
 
-}
-
-func initCors() {
-	// 跨域
-	corsPlugin := cors.NewPlugin(
-		cors.WithAllowMethods(http.MethodHead, http.MethodGet, http.MethodPost, http.MethodPut, http.MethodPatch, http.MethodDelete),
-		cors.WithAllowCredentials(true),
-		cors.WithMaxAge(3600),
-		cors.WithUseRsPkg(true),
-	)
-	api.Register(corsPlugin)
 }
