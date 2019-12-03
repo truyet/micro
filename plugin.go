c7b28546086baaf85d75a0e8da439ca57ed59176
Edward <chenlulei@gmail.com>
2019-12-03 23:09:56 +0800

    merge config of casbin & and auth

--- plugin.go
+++ plugin.go
@@ -2,15 +2,20 @@ package main
 
 import (
 	"io"
+	"net/http"
+	fileadapter	"github.com/casbin/casbin/v2/persist/file-adapter"
+	//"github.com/micro/go-micro/util/log"
+	"github.com/micro/micro/plugin/micro/auth"
+	"github.com/micro/micro/plugin/micro/util/response"
+	"github.com/micro/micro/api"
 )
 
 var (
-	apiTracerCloser, webTracerCloser io.Closer
+	apiTracerCloser io.Closer
 )
 
 func cleanWork() error {
 	// closer
-	webTracerCloser.Close()
 	apiTracerCloser.Close()
 
 	return nil
@@ -18,5 +23,23 @@ func cleanWork() error {
 
 // 插件注册
 func init() {
+	// Auth
+	initAuth()
+}
+
+
+func initAuth() {
+
+	casb := fileadapter.NewAdapter("./conf/casbin_policy.csv")
+	auth.RegisterAdapter("default", casb)
+
+
+	authPlugin := auth.NewPlugin(
+		auth.WithResponseHandler(response.DefaultResponseHandler),
+		auth.WithSkipperFunc(func(r *http.Request) bool {
+			return false
+		}),
+	)
+	api.Register(authPlugin)
 
 }
